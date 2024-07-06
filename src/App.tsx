f9710617e4048be9bbdbc8ab84b14be91765adc7
julia moraes <juliademoraess@gmail.com>
2024-07-06 02:53:19 -0300

    chore: basecode

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/base.scss';
+import { Navbar } from './components/NavBar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
