a425591b26c32e99421cb2fe368d0957bee41c5f
julia moraes <juliademoraess@gmail.com>
2024-07-06 02:53:38 -0300

    feat: navbar component

---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,18 @@
+import './Navbar.scss';
+
+export function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <div className="navbar__icon">
+          <span>Júlia Moraes</span>
+        </div>
+        <div className="navbar__topics">
+          <li>Sobre mim</li>
+          <li>Projetos</li>
+          <li>Contatos</li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
